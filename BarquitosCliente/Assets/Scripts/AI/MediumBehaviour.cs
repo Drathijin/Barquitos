45f4cb944ffa83d956f2db7df66af69a8abb010f
Drathijin <rsulbara@ucm.es>
2021-06-14 23:20:42 +0200

    Dificultad media funcional

--- BarquitosCliente/Assets/Scripts/AI/MediumBehaviour.cs
+++ BarquitosCliente/Assets/Scripts/AI/MediumBehaviour.cs
@@ -11,7 +11,8 @@ public class MediumBehaviour : IABehaviour
 	Vector2Int hit_;
 	Vector2Int[] directions_;
 	List<Vector2Int> possibleBoats_;
-	List<Vector2Int> hitHistory_;
+	bool[,] hitHistory_;
+
 	private void Start() {
 		generator_ = new System.Random();
 		positions_ = new List<int>();
@@ -21,18 +22,21 @@ public class MediumBehaviour : IABehaviour
 		directions_[2] = new Vector2Int(0,-1);
 		directions_[3] = new Vector2Int(0,1);
 		possibleBoats_ = new List<Vector2Int>();
+		hitHistory_ = new bool[10,10];
+		for(int i = 0; i<10;i++)
+			for(int j = 0; j<10;j++)
+				hitHistory_[i,j] = false;
 		CheckerBoardPositionSet();
 	}
 
 	private bool checkHit()
 	{
-		Debug.LogError(fleet_.GetGrid().GetPos(hit_.x,hit_.y).Data().State());
 		if(fleet_.GetGrid().GetPos(hit_.x,hit_.y).Data().State() == CellData.CellState.HIT)
-			Debug.LogError("ASSSASASASASASASDADSDASDSAD");
+			hitHistory_[hit_.x, hit_.y] = true;
 		return fleet_.GetGrid().GetPos(hit_.x,hit_.y).Data().State() == CellData.CellState.HIT;
 	}
 	//Saves board positions in a checkerboard pattern
-		private void CheckerBoardPositionSet()
+	private void CheckerBoardPositionSet()
 	{
 		//Add all even numbers
 		Debug.Log("|| POSITIONS ||");
@@ -79,25 +83,34 @@ public class MediumBehaviour : IABehaviour
 	}
 	private void destroy()
 	{
+		//Pick one from the possible list
+		int position = generator_.Next(0,possibleBoats_.Count);
+		Vector2Int vPosition = possibleBoats_[position];
+		possibleBoats_.RemoveAt(position);
+		positions_.Remove(vPosition.x+vPosition.y*10);
+		hit_.x = vPosition.x;
+		hit_.y = vPosition.y;
+	}
+	private void ConfirmHit()
+	{
 		//We landed a hit, add all other posible cells to de possible list
 		if(checkHit())
 		{
 			foreach (var dir in directions_)
 			{
-				if(hit_.x+dir.x >= 0 && dir.x +hit_.x < 10 && hit_.y+dir.y >= 0 && dir.y +hit_.y < 10)
-					if(possibleBoats_.Find(x => (x.x==(hit_+dir).x)&&(x.y == (hit_+dir).y)) != new Vector2Int() || hit_+dir == new Vector2Int())
+				Vector2Int res = hit_+dir;
+				if((res.x >= 0 && res.x < 10 && res.y >= 0 && res.y < 10)&&
+					!hitHistory_[res.x,res.y])
+					{
+						Debug.Log("Adding: "+res.x+" "+res.y);
 						possibleBoats_.Add(hit_+dir);
+					}
 			}
 		}
-		//Pick one from the possible list
-		int position = generator_.Next(0,possibleBoats_.Count);
-		Vector2Int vPosition = possibleBoats_[position];	
-		possibleBoats_.RemoveAt(position);
-		hit_.x = vPosition.x;
-		hit_.y = vPosition.y;
 	}
 	public override AttackData Attack(){
 		fleet_ = SelectTarget();
+		ConfirmHit();
 		if(!seek())
 			destroy();
 
